7296f40761da442a5f63dec687a9319993d474fc
Minachev <69713370+Minachev@users.noreply.github.com>
2021-04-13 14:25:18 +0300

    added smth

--- Playstore/Playstore/Controllers/HomeController.cs
+++ Playstore/Playstore/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Playstore.Controllers
             return View();
         }
 
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
