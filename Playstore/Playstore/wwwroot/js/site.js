bf380321378e4d5612a1487412e2348a07ec7b0f
Sylu <liccy.sy@yandex.ru>
2021-04-17 00:15:57 +0300

    fixed carousel

--- Playstore/Playstore/wwwroot/js/site.js
+++ Playstore/Playstore/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿
+(function ($) {
+    jQuery(document).ready(function ($) {
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            singleItem: true,
+            itemsScaleUp: true,
+            navigation: true,
+            navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            pagination: true,
+            merge: false,
+            mergeFit: true,
+            slideBy: 1,
+            autoPlay: false
+        });
+    });
+})(jQuery);
+
