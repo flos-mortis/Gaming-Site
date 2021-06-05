ee6987f2f783a22db0825b613c68a7825b83c601
Sylu <liccy.sy@yandex.ru>
2021-06-05 06:44:45 +0300

    fixed

--- TestStore/TestStore/Controllers/ShopController.cs
+++ TestStore/TestStore/Controllers/ShopController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestStore.Models;
 
 namespace TestStore.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly ILogger<ShopController> _logger;
+        private ApplicationContext db;
+        public ShopController(ILogger<ShopController> logger, ApplicationContext db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,6 +31,22 @@ namespace TestStore.Controllers
         }
         public IActionResult Payment()
         {
+            return View();
+        }
+        public IActionResult Action(string sortOrder)
+        {
+            ViewBag.PriceSortParam = String.IsNullOrEmpty(sortOrder) ? "price_desc" : "";
+            var games = from b in db.Games select b;
+
+            switch (sortOrder)
+            {
+                case "price_desc":
+                    games = games.OrderByDescending(b => b.Price);
+                    break;
+                default:
+                    games = games.OrderBy(b => b.Price);
+                    break;
+            }
             return View();
         }
     }
