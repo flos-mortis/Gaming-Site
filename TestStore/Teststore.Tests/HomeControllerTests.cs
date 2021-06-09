f6be843ffa4253083212396270843bf6b7270ec3
Sylu <liccy.sy@yandex.ru>
2021-06-09 14:33:21 +0300

    added unit tests

---
+++ TestStore/Teststore.Tests/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Teststore.Controllers;
+
+namespace Teststore.Tests
+{
+    class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            HomeController controller = new HomeController()
+        }
+    }
+}
